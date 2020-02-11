ad10651149d221f1d7002e6732eb7ed934b73082
Dmitry <dmitry.martynenko92@gmail.com>
2020-02-11 15:30:43 +0300

    добавлено отображение модели Contact в шаблон persons.html

--- mysite/phonebook/views.py
+++ mysite/phonebook/views.py
@@ -1,8 +1,9 @@
 # from django.http import HttpResponse
 from django.shortcuts import render, HttpResponse
-from phonebook.models import Person
+from phonebook.models import Person, Contact
 
 def index(request):
     person = Person.objects.all()
-    args = {'persons':person}
+    contact = Contact.objects.all()
+    args = {'persons':person, 'contacts':contact}
     return render(request, 'persons/persons.html', args)
