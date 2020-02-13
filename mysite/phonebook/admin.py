ff88bc3fbfc9f057049ddeea76dd47ad5d55adf1
Dmitry <dmitry.martynenko92@gmail.com>
2020-02-13 22:12:03 +0300

    добавлены изображения и пересоздана база данных

--- mysite/phonebook/admin.py
+++ mysite/phonebook/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from phonebook.models import Person
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display=('first_name', 'last_name', 'department', 'mobile_number', 'extension_number', 'department_position', 'email')
-    list_display_links = ('first_name', 'last_name')
-    list_editable=('department', 'mobile_number', 'extension_number', 'department_position', 'email')
+    list_display=('first_name', 'middle_name', 'last_name', 'department', 'mobile_number', 'extension_number', 'department_position', 'email')
+    list_display_links = ('first_name', 'middle_name')
+    list_editable=('last_name', 'department', 'mobile_number', 'extension_number', 'department_position', 'email')
 
 admin.site.register(Person, PersonAdmin)
