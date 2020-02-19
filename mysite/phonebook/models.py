eada5b324cfe6c9386082afb7fcd5d1656b42cab
Dmitry <dmitry.martynenko92@gmail.com>
2020-02-19 22:21:12 +0300

    добавлена сортировка по фамилии

--- mysite/phonebook/models.py
+++ mysite/phonebook/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    middle_name = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True)
     mobile_number = models.CharField(max_length=11, blank=True)
     extension_number = models.IntegerField(blank=True)
-    department = models.CharField(max_length=30, blank=True)
-    department_position = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=30, blank=True)
-
-
-    def __str__(self):
-        return self.first_name
+    department = models.CharField(max_length=255, blank=True)
+    department_position = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
